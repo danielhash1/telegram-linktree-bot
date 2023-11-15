a85fde00d23d75957f51e526baa4c8042d46fbb0
danielhash1 <108074788+danielhash1@users.noreply.github.com>
2023-11-15 11:49:57 +0100

    Fix

---
+++ script.py
@@ -0,0 +1,13 @@
+# from telegram import Bot
+
+# # Замените 'YOUR_BOT_TOKEN' на фактический токен вашего бота
+# bot = Bot(token='6985329095:AAHaq0BZnHKlVhVtmhdIiAetumQaJEadk3o')
+
+# # Замените 'YOUR_CHAT_ID' на фактический идентификатор чата вашего бота
+# chat_id = ''
+
+# # Замените 'YOUR_COMMAND' на фактическую команду, которую вы хотите отправить
+# command = '/cf7tg_start'
+
+# # Отправка команды в чат бота
+# bot.send_message(chat_id=chat_id, text=command)
